b7e61e6ba55c4301114abc78f52c0e0c636daa2f
Alan <alanbritodossantos2015@gmail.com>
2022-09-05 00:26:14 -0300

    hangs-on 11

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,11 +8,12 @@ import { first, last } from 'rxjs';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements DoCheck {
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   constructor() {}
   ngDoCheck(): void {
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    // this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorate();
   }
 
   public setEmitTaskList(event:string){
@@ -35,6 +36,13 @@ export class TodoListComponent implements DoCheck {
 
       if(confirm)
         this.deleteItemTaskList(index);
+    }
+  }
+
+  public setLocalStorate(){
+    if(this.taskList){
+      this.taskList.sort((first,last)=>Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list",JSON.stringify(this.taskList))
     }
   }
 }
